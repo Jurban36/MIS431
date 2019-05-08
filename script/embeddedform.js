44fe43afdcd645b09b694026b4ded371f7ddc335
Jurban36 <Jennymurban@gmail.com>
2019-05-08 13:00:57 -0400

    Elev8 registration page with header and footer

--- script/embeddedform.js
+++ script/embeddedform.js
@@ -1,15 +1,19 @@
 var myFunction = function(){
-    console.log("Hi")
-    document.getElementById("header").innerText = "Individual Form" 
-    document.getElementById("displaydiv").style.height = "450px";
-    var testing = "<iframe class='airtable-embed' src='https://airtable.com/embed/shr74LTphnHoblAOd?backgroundColor=yellow' frameborder='0' onmousewheel='' width='100%' height='533' style='background: transparent; border: 1px solid #ccc;'></iframe>"
-    document.getElementById("displaydiv").innerHTML = testing;
-    document.getElementById("buttons").style.visibility = "hidden";
+    // document.getElementById("header").innerText = "Individual Form" 
+
+    document.getElementById("header").remove()
+    document.getElementById("smallheader").remove()
+    document.getElementById("text").style.height = "470px";
+    var testing = "<iframe class='airtable-embed' src='https://airtable.com/embed/shr74LTphnHoblAOd?backgroundColor=yellow' frameborder='0' onmousewheel='' width='100%' height='100%' style='background: transparent; border: 1px solid #ccc;'></iframe>"
+    document.getElementById("text").innerHTML = testing;
+    // document.getElementById("buttons").style.visibility = "hidden";
 }
 var functionTwo = function(){
-    document.getElementById("header").innerText = "Company Form" ;
-    document.getElementById("displaydiv").style.height = "450px";
-    var testing = "<iframe class='airtable-embed' src='https://airtable.com/embed/shr1j7jk2014WddiJ?backgroundColor=yellow' frameborder='0' onmousewheel='' width='100%' height='533' style='background: transparent; border: 1px solid #ccc;'></iframe>"
-    document.getElementById("displaydiv").innerHTML = testing;
-    document.getElementById("buttons").style.visibility = "hidden";
+    // document.getElementById("header").innerText = "Company Form" ;
+    document.getElementById("header").remove()
+    document.getElementById("smallheader").remove()
+    document.getElementById("text").style.height = "470px";
+    var testing = "<iframe class='airtable-embed' src='https://airtable.com/embed/shr1j7jk2014WddiJ?backgroundColor=yellow' frameborder='0' onmousewheel='' width='100%' height='100%' style='background: transparent; border: 1px solid #ccc;'></iframe>"
+    document.getElementById("text").innerHTML = testing;
+    // document.getElementById("buttons").style.visibility = "hidden";
 }
